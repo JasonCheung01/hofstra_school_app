c95e04d1e146def341f01c303f70b414755ebc0e
Jason Cheung <jasoncheung@Jasons-MacBook-Pro.local>
2021-10-16 23:57:51 -0400

    finish creating a basic profile page

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -8,22 +8,20 @@ class profilePage extends StatefulWidget {
 class _ProfilePageState extends State<profilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage("lib/img/test.jpg"),
-            ),
-            SizedBox(height: 14.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                    child: Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 60,
+            backgroundImage: AssetImage("lib/img/test.jpg"),
+          ),
+          // Creates space between pfp pic and description
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
                   children: [
                     Text("Name: Jason Cheung",
                         style: TextStyle(
@@ -42,12 +40,58 @@ class _ProfilePageState extends State<profilePage> {
                           fontSize: 14.0,
                         )),
                   ],
-                ))
-              ],
-            )
-          ],
-        ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
 }
+// class _ProfilePageState extends State<profilePage> {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: new Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             CircleAvatar(
+//               radius: 60,
+//               backgroundImage: AssetImage("lib/img/test.jpg"),
+//             ),
+//             SizedBox(height: 14.0),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Expanded(
+//                     child: Column(
+//                   children: [
+//                     Text("Name: Jason Cheung",
+//                         style: TextStyle(
+//                           fontSize: 14.0,
+//                         )),
+//                     Text("Major: Computer Science",
+//                         style: TextStyle(
+//                           fontSize: 14.0,
+//                         )),
+//                     Text("Email: jcheung1@pride.hofstra.edu",
+//                         style: TextStyle(
+//                           fontSize: 14.0,
+//                         )),
+//                     Text("Birthday: October 21, 2001",
+//                         style: TextStyle(
+//                           fontSize: 14.0,
+//                         )),
+//                   ],
+//                 ))
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
