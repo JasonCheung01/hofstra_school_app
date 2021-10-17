58892d11bd3d7751cec97d1020d00e465fd07101
Jason Cheung <jasoncheung@Jasons-MacBook-Pro.local>
2021-10-16 22:09:02 -0400

    implemented basic functionality of a calendar

--- lib/calendarPage.dart
+++ lib/calendarPage.dart
@@ -13,13 +13,78 @@ class _CalendarPageState extends State<calendarPage> {
         body: Container(
           child: SfCalendar(
             view: CalendarView.month,
+            initialSelectedDate: DateTime.now(),
             monthViewSettings: MonthViewSettings(
               showAgenda: true,
               agendaItemHeight: 70,
             ),
+            // dataSource: MeetingDataSource(),
           ),
         ),
       ),
     );
   }
 }
+
+// class MeetingDataSource extends CalendarDataSource {
+//   MeetingDataSource(List<Meeting> source){
+//     appointments = source;
+//   }
+
+//   @override
+//   DateTime getStartTime(int index) {
+//     return appointments![index].from;
+//   }
+
+//   @override
+//   DateTime getEndTime(int index) {
+//     return appointments![index].to;
+//   }
+
+//   @override
+//   bool isAllDay(int index) {
+//     return appointments![index].isAllDay;
+//   }
+
+//   @override
+//   String getSubject(int index) {
+//     return appointments![index].eventName;
+//   }
+
+//   @override
+//   String getStartTimeZone(int index) {
+//     return appointments![index].startTimeZone;
+//   }
+
+//   @override
+//   String getEndTimeZone(int index) {
+//     return appointments![index].endTimeZone;
+//   }
+
+//   @override
+//   Color getColor(int index) {
+//     return appointments![index].background;
+//   }
+// } 
+
+// // This is temporary
+// // This creates a meeting for us automatically with these requirements
+// _AppointmentDataSource _getCalendarDataSource() {
+//   List<Appointment> appointments = <Appointment>[];
+//   appointments.add(Appointment(
+//     startTime: DateTime.now(),
+//     endTime: DateTime.now().add(Duration(minutes: 10)),
+//     subject: 'Meeting',
+//     color: Colors.blue,
+//     startTimeZone: '',
+//     endTimeZone: '',
+//   ));
+
+//   return _AppointmentDataSource(appointments);
+// }
+
+// class _AppointmentDataSource extends CalendarDataSource {
+//   _AppointmentDataSource(List<Appointment> source) {
+//     appointments = source;
+//   }
+// }
